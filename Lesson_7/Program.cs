a5dc038551b1797e7caeed3698311c04bfd191fb
Andrey <twintoo@yandex.ru>
2022-11-29 12:04:19 +0300

    Lesson_7

---
+++ Lesson_7/Program.cs
@@ -0,0 +1,36 @@
+﻿int rows = ReadInt("Введите количество строк: ");
+int colums = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[rows, colums];
+
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
+        {
+            array[i, j] = new Random().NextDouble();
+        }
+    }
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
